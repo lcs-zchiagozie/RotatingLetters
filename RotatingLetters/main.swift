daf42744f2f8fc090c6a7fd685f0c85a7ff98c06
Chiagozie, Zachary <zchiagozie@lcs-mba3000.lcs.on.ca>
2018-04-10 14:55:39 -0400

    Rotating Signs, going good. Made the input part.

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
+// INPUT
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+// Loop forever until we get good input
+while 1 == 1 {
+    // Prompt the user
+    print ("Input word is?")
+    
+    //Wait for input
+    guard let givenInput = readLine() else {
+        
+        // If we got here, input is nil
+        //Prompt again
+        continue // continue with the next iteration of while loop
+    }
+
+    // Test #2: Is the input the correct length
+    if givenInput.count < 1 || givenInput.count > 30 {
+        
+        // If we got here, input is short
+        continue // prompt again
+    }
+    
+    // If we got to this point, we know the input is good
+    break
+    
+}
 
